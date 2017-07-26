3b0f55f1238c895edcee49968c9cdef2188b4211
Javier Cabezas <jcleyton@gmail.com>
2017-07-26 19:40:40 -0400

    Added logo and changed welcome text

--- src/components/sections/Welcome.vue
+++ src/components/sections/Welcome.vue
@@ -4,8 +4,8 @@
             <div class="slide_text">
                 <div class="table">
                     <div class="table_cell">
-                        <h2>M&S CONSULTORES</h2>
-                        <h3>Poner un slogan aquí</h3>
+                        <h2>M&S CONSULTING GROUP</h2>
+                        <h3>Making a difference</h3>
                         <a class="btn btn-default" href="#" role="button">Obtén una cotización</a>
                     </div>
                 </div>
